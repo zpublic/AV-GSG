21df71a49ac78d4df3909ae526e21a29cb74fd40
zapline <278998871@qq.com>
2013-03-25 01:04:14 +0800

    * 整理格式

--- AV-CSG/OggFile.h
+++ AV-CSG/OggFile.h
@@ -4,26 +4,26 @@
 class OggFile : public AudioInput
 {
 public:
-	OggFile(const std::string& file_name);
+    OggFile(const std::string& file_name);
 
-	~OggFile();
+    ~OggFile();
 
-	bool Initialize();
+    bool Initialize();
 
-	void Seek(uint32 sample_position);
+    void Seek(uint32 sample_position);
 
-	uint32 Read(uint8* data_buffer, uint32 number_samples, bool& end);
+    uint32 Read(uint8* data_buffer, uint32 number_samples, bool& end);
 
 private:
-	bool _initialized;
+    bool _initialized;
 
-	OggVorbis_File _vorbis_file;
+    OggVorbis_File _vorbis_file;
 
-	unsigned char _read_buffer[4096];
+    unsigned char _read_buffer[4096];
 
-	uint16 _read_buffer_position;
+    uint16 _read_buffer_position;
 
-	uint16 _read_buffer_size;
+    uint16 _read_buffer_size;
 
-	static int _FileSeekWrapper(FILE* file, ogg_int64_t off, int whence);
+    static int _FileSeekWrapper(FILE* file, ogg_int64_t off, int whence);
 };
