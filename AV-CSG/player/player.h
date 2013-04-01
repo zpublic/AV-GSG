4f3891aad41e6b51d3827ad86f3fb29b26898147
zapline <278998871@qq.com>
2013-04-01 15:29:42 +0800

    * 玩家类框架

--- AV-CSG/player/player.h
+++ AV-CSG/player/player.h
@@ -1,2 +1,20 @@
 #pragma once
+#include "unit/Singleton.h"
+#include "player_gamestatus.h"
+#include "player_property.h"
+#include "player_savedata.h"
+#include "player_skill.h"
+
+class Player : public Singleton<Player>
+{
+    friend class Singleton<Player>;
+public:
+    Player();
+    ~Player();
+
+private:
+
+public:
+
+};
 
--- AV-CSG/player/player_skill.h
+++ AV-CSG/player/player_skill.h
@@ -1,2 +1,6 @@
 #pragma once
 
+class Player_Skill
+{
+
+};
