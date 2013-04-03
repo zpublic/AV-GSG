b0e88b306eda07e4bc5aae3730bc5596cdd428e5
Sum <308974268@qq.com>
2013-04-04 01:51:50 +0800

    修改场景的图片载入方式

--- AV-CSG/scene/GameScene_Play.h
+++ AV-CSG/scene/GameScene_Play.h
@@ -1,11 +1,12 @@
 #pragma once
 #include "GameScene.h"
 #include "control\stage_player\GameStagePlayer.h"
+#include "data\resourcedata\Picture.h"
 
 class GameScene_Play : public GameScene
 {
 public:
-    GameScene_Play(const TCHAR* lpszPath, HDC BkDc);
+    GameScene_Play(const TCHAR* lpszPath);
     virtual ~GameScene_Play();
 
     virtual void Update();
@@ -15,7 +16,7 @@ private:
     GameScene_Play();
     void ControlSelfPlane();
     void _CirculationMap();
-    bool _LoadImage(const TCHAR* lpszPath, HDC hDC);
+    CPicture m_Picture;
     HDC m_BackgourdDC;
     int m_nY;
 };
