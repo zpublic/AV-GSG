1813f88f044f8d18586ad2f12088634623dfe469
zapline <278998871@qq.com>
2013-04-05 16:57:32 +0800

    + 增加游戏状态

--- AV-CSG/player/player_gamestatus.cpp
+++ AV-CSG/player/player_gamestatus.cpp
@@ -2,7 +2,10 @@
 #include "player_gamestatus.h"
 
 Player_GameStatus::Player_GameStatus()
-	: m_nMaxScore(0)
+    : m_nMaxScore(0)
+    , m_nLife(Default_Life)
+    , m_HP(0)
+    , m_nScroe(0)
 {
 }
 
@@ -12,15 +15,92 @@ Player_GameStatus::~Player_GameStatus()
 
 int Player_GameStatus::GetMaxScore()			
 {
-	return m_nMaxScore;
+    return m_nMaxScore;
 }
-	
+
 bool Player_GameStatus::SetMaxScore(int nMaxScore)
 {
-	if(nMaxScore <= 0 || nMaxScore < m_nMaxScore)
-	{
-		return false;
-	}
-	m_nMaxScore = nMaxScore;
-	return true;
+    if(nMaxScore <= 0 || nMaxScore < m_nMaxScore)
+    {
+        return false;
+    }
+    m_nMaxScore = nMaxScore;
+    return true;
+}
+
+int Player_GameStatus::GetLife()
+{
+    return m_nLife;
+}
+
+int Player_GameStatus::AddLife( int nAdd /*= 0*/ )
+{
+    m_nLife += nAdd;
+    return m_nLife;
+}
+
+int Player_GameStatus::SubLife( int nSub /*= 0*/ )
+{
+    m_nLife -= nSub;
+    if (m_nLife < 0)
+    {
+        m_nLife = 0;
+        assert(false);
+    }
+    return m_nLife;
+}
+
+int Player_GameStatus::GetHp()
+{
+    return m_HP;
+}
+
+void Player_GameStatus::SetHp( int nHp )
+{
+    m_nLife = nHp;
+}
+
+int Player_GameStatus::AddHp( int nAdd /*= 0*/ )
+{
+    m_HP += nAdd;
+    return m_HP;
+}
+
+int Player_GameStatus::SubHp( int nSub /*= 0*/ )
+{
+    m_HP -= nSub;
+    if (m_HP < 0)
+    {
+        m_HP = 0;
+        assert(false);
+    }
+    return m_HP;
+}
+
+int Player_GameStatus::GetScore()
+{
+    return m_nScroe;
+}
+
+int Player_GameStatus::AddScore( int nAdd /*= 0*/ )
+{
+    m_nScroe += nAdd;
+    return m_nScroe;
+}
+
+int Player_GameStatus::SubScore( int nSub /*= 0*/ )
+{
+    m_nScroe -= nSub;
+    if (m_nScroe < 0)
+    {
+        m_nScroe = 0;
+        assert(false);
+    }
+    return m_nScroe;
+}
+
+void Player_GameStatus::ResetGameStatus()
+{
+    m_nLife = Default_Life;
+    m_nScroe = 0;
 }
--- AV-CSG/player/player_gamestatus.h
+++ AV-CSG/player/player_gamestatus.h
@@ -1,16 +1,44 @@
 #pragma once
 
+#define Default_Life    1
+
 class Player_GameStatus
 {
 public:
-	Player_GameStatus();
-	~Player_GameStatus();
+    Player_GameStatus();
+    ~Player_GameStatus();
 
 public:
-	int GetMaxScore();
-	bool SetMaxScore(int nMaxScore);
+    ///> 单局最高得分
+    int GetMaxScore();
+    bool SetMaxScore(int nMaxScore);
+
+    ///> 剩余生命
+    int GetLife();
+    int AddLife(int nAdd = 0);
+    int SubLife(int nSub = 0);
+
+    ///> 血量
+    int GetHp();
+    void SetHp(int nHp);
+    int AddHp(int nAdd = 1);
+    int SubHp(int nSub = 1);
+
+    ///> 当前得分
+    int GetScore();
+    int AddScore(int nAdd = 0);
+    int SubScore(int nSub = 0);
+
+    ///> 重置游戏状态
+    void ResetGameStatus();
 
 private:
-	///> 玩家打飞机的单局最高得分
-	int m_nMaxScore;  
+    ///> 玩家打飞机的单局最高得分
+    int m_nMaxScore;
+    ///> 当前得分
+    int m_nScroe;
+    ///> 剩余血量
+    int m_HP;
+    ///> 剩余生命条数
+    int m_nLife;
 };
