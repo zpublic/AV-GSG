e2f897965cd196e32ad32d82a830ba1b2da6a08a
YeZi <645059545@qq.com>
2013-04-05 16:08:49 +0800

    add some functions

--- AV-CSG/player/player_gamestatus.cpp
+++ AV-CSG/player/player_gamestatus.cpp
@@ -2,7 +2,10 @@
 #include "player_gamestatus.h"
 
 Player_GameStatus::Player_GameStatus()
-	: m_nMaxScore(0)
+	: m_nPlayerMaxScore(0)
+    , m_nPlayerHp(0)
+    , m_nPlayerLifeNum(0)
+    , m_nPlayerCurrentScore(0)
 {
 }
 
@@ -10,17 +13,65 @@ Player_GameStatus::~Player_GameStatus()
 {
 }
 
-int Player_GameStatus::GetMaxScore()			
+bool Player_GameStatus::SetPlayerMaxScore(int nPlayerMaxScore)
 {
-	return m_nMaxScore;
-}
-	
-bool Player_GameStatus::SetMaxScore(int nMaxScore)
-{
-	if(nMaxScore <= 0 || nMaxScore < m_nMaxScore)
+	if(nPlayerMaxScore <= 0 || nPlayerMaxScore < m_nPlayerMaxScore)
 	{
 		return false;
 	}
-	m_nMaxScore = nMaxScore;
+	m_nPlayerMaxScore = nPlayerMaxScore;
 	return true;
+}
+
+int Player_GameStatus::GetPlayerMaxScore()			
+{
+	return m_nPlayerMaxScore;
+}
+
+
+bool Player_GameStatus::SetPlayerHp(int nPlayerHp)
+{
+    if(nPlayerHp < 0)
+    {
+        return false;
+    }
+
+    m_nPlayerHp = nPlayerHp;
+}
+    
+int Player_GameStatus::GetPlayerHp()
+{
+    return m_nPlayerHp;
+}
+
+
+bool Player_GameStatus::SetPlayerLifeNum(int nPlayerLifeNum)
+{
+    if(nPlayerLifeNum < 0)
+    {
+        return false;
+    }
+
+    m_nPlayerLifeNum = nPlayerLifeNum;
+}
+
+int Player_GameStatus::GetPlayerLifeNum()
+{
+    return m_nPlayerLifeNum;
+}
+
+
+bool Player_GameStatus::SetPlayerCurrentScore(int nPlayerCurrentScore)
+{
+    if(nPlayerCurrentScore < 0 && nPlayerCurrentScore < m_nPlayerCurrentScore)
+    {
+        return false;
+    }
+
+    m_nPlayerCurrentScore = nPlayerCurrentScore;
+}
+
+int Player_GameStatus::GetPlayerCurrentScore()
+{
+    return m_nPlayerCurrentScore;
 }
